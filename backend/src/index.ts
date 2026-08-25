beafab22d26f77f2bfe359c802eaf47b80dc8300
phuonganhngo95 <phanhzu95@gmail.com>
2026-08-25 16:50:09 +0700

    add cron job health check, update dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,7 @@ import path from "node:path";
 import { clerkMiddleware } from "@clerk/express";
 import { clerkWebhookHandler } from "./webhooks/clerk";
 import { getEnv } from "./lib/env";
+import keepAliveCron from "./lib/cron";
 
 const env = getEnv()
 const app = express();
@@ -24,6 +25,10 @@ app.post("/webhooks/clerk", rawJson, (req, res) => {
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
+
+app.get("/health", (_req, res) => {
+    res.json( {ok: true});
+});
 
 const publicDir = path.join(process.cwd(), "public");
 if (fs.existsSync(publicDir)) {
@@ -44,4 +49,10 @@ if (fs.existsSync(publicDir)) {
     });
 }
 
-app.listen(env.PORT, () => console.log("listening on port: ", env.PORT));
+app.listen(env.PORT, () => {
+    console.log("Listening on port: ", env.PORT)
+
+    if (env.NODE_ENV === "production") {
+        keepAliveCron.start();
+    }
+});
