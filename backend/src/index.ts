0921bc2a5ffd646f2dcaa75fe1ba7c2292afabda
phuonganhngo95 <phanhzu95@gmail.com>
2026-08-29 20:23:26 +0700

    polar webhooks

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,7 @@ import productRouter from "./routes/productRouter";
 import meRouter from "./routes/meRouter";
 import streamRouter from "./routes/streamRouter";
 import checkoutRouter from "./routes/checkoutRouter";
+import { polarWebhookHandler } from "./webhooks/polar";
 
 const env = getEnv()
 const app = express();
@@ -27,9 +28,9 @@ app.post("/webhooks/clerk", rawJson, (req, res) => {
     void clerkWebhookHandler(req, res);
 });
 
-// app.post("/webhooks/polar", rawJson, (req, res) => {
-//     void polarWebhookHandler(req, res);
-// });
+app.post("/webhooks/polar", rawJson, (req, res) => {
+    void polarWebhookHandler(req, res);
+});
 
 app.use(express.json());
 app.use(cors());
