938eb71adcac8ddf4d3db8e4f05af592a94c0c37
phuonganhngo95 <phanhzu95@gmail.com>
2026-09-01 17:02:59 +0700

    admin routes, controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,6 +16,8 @@ import productRouter from "./routes/productRouter";
 import meRouter from "./routes/meRouter";
 import streamRouter from "./routes/streamRouter";
 import checkoutRouter from "./routes/checkoutRouter";
+import adminRouter from "./routes/adminRouter";
+
 import { polarWebhookHandler } from "./webhooks/polar";
 import { sentryClerkUserMiddleware } from "./middleware/sentryClerkUser";
 
@@ -48,6 +50,7 @@ app.use("api/me", meRouter);
 app.use("api/products", productRouter);
 app.use("api/stream", streamRouter);
 app.use("api/checkout", checkoutRouter);
+app.use("api/admin", adminRouter);
 
 const publicDir = path.join(process.cwd(), "public");
 if (fs.existsSync(publicDir)) {
