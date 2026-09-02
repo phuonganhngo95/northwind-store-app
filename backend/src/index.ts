56412a15b40bc353202cf4fcc3f1e032d68fc0fc
phuonganhngo95 <phanhzu95@gmail.com>
2026-09-02 15:47:56 +0700

    order routes, controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,6 +17,7 @@ import meRouter from "./routes/meRouter";
 import streamRouter from "./routes/streamRouter";
 import checkoutRouter from "./routes/checkoutRouter";
 import adminRouter from "./routes/adminRouter";
+import orderRouter from "./routes/orderRouter";
 
 import { polarWebhookHandler } from "./webhooks/polar";
 import { sentryClerkUserMiddleware } from "./middleware/sentryClerkUser";
@@ -51,6 +52,7 @@ app.use("api/products", productRouter);
 app.use("api/stream", streamRouter);
 app.use("api/checkout", checkoutRouter);
 app.use("api/admin", adminRouter);
+app.use("api/orders", orderRouter);
 
 const publicDir = path.join(process.cwd(), "public");
 if (fs.existsSync(publicDir)) {
