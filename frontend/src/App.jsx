4b5871de2f6ea356f95db61c276f464ebb6d5cb9
phuonganhngo95 <phanhzu95@gmail.com>
2026-09-03 16:59:04 +0700

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,29 @@
-import './App.css';
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react';
+import { Show, SignInButton, SignUpButton, useAuth, UserButton } from '@clerk/react';
+import PageLoader from './components/PageLoader';
+import Layout from './components/Layout';
 
 function App() {
+  const { isLoaded } = useAuth();
+
+  if (!isLoaded) return <PageLoader />;
+
   return (
-    <>
+    <Layout>
       <header>
         <Show when="signed-out">
-          <SignInButton mode='modal'/>
-          <SignUpButton mode='modal'/>
+          <SignInButton mode="modal" />
+          <SignUpButton mode="modal" />
         </Show>
         <Show when="signed-in">
           <UserButton />
         </Show>
       </header>
-    </>
+
+      <p className="text-red-500 font-extrabold text-4xl bg-blue">Hello</p>
+      <button className="btn btn-primary">Click me</button>
+      <button className="btn btn-secondary">Click me</button>
+      <button className="btn btn-outline">Click me</button>
+    </Layout>
   )
 }
 
