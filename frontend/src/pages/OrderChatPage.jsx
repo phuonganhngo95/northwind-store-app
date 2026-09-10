10bda17cce7e72ae34ec7be87396b5eacaa7bcd6
phuonganhngo95 <phanhzu95@gmail.com>
2026-09-10 17:02:28 +0700

    setup and create order-chat page

--- frontend/src/pages/OrderChatPage.jsx
+++ frontend/src/pages/OrderChatPage.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { OrderChatPanelSkeleton } from "../components/LoadingSkeletons";
+import { PageError } from "../components/PageError";
+import { useOrderChatPage } from "../hooks/useOrderChatPage";
 
 function OrderChatPage() {
+  const { paid, client, error, channel, canInvite, inviteMutation } = useOrderChatPage();
+
+  if (!paid) {
+    return <p className="text-base-content/60">Complete payment to open support chat.</p>;
+  }
+  if (error) {
+    return <PageError message={error} />;
+  }
+  if (!client || !channel) {
+    return <OrderChatPanelSkeleton />;
+  }
+
   return (
-    <div>
-      
+    <div className="space-y-4 text-left">
+      <div>
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default OrderChatPage
+export default OrderChatPage;
