424e026f0a7e6bdd24d1a42e06ae0b2c69746cf6
phuonganhngo95 <phanhzu95@gmail.com>
2026-09-11 16:15:44 +0700

    add order-video page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import { SentryDemoPage } from './pages/SentryDemoPage';
 import OrderDetailPage from './pages/OrderDetailPage';
 import OrderSummaryPage from './pages/OrderSummaryPage';
 import OrderChatPage from './pages/OrderChatPage';
+import OrderVideoPage from './pages/OrderVideoPage';
 
 function App() {
   const { isLoaded, isSignedIn } = useAuth();
@@ -27,8 +28,12 @@ function App() {
           path="/orders"
           element={isSignedIn ? <OrdersPage /> : <Navigate to={"/"} replace />}
         />
+
         <Route path="/checkout/return" element={<CheckoutReturnPage />} />
+
         <Route path="/demo-sentry" element={<SentryDemoPage />} />
+
+        <Route path="/orders/:id/call" element={isSignedIn ? <OrderVideoPage /> : <Navigate to={"/"} replace />} />
 
         {/* NESTED ROUTES */}
         <Route path="/orders/:id" element={<OrderDetailPage />}>
